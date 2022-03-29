1ea6cbbefc11611e29d59135495f27e474f7c710
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-03-29 20:35:21 -0300

    fix: jest config

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+export default {
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '<rootDir>/tests/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/tests/e2e/cypress'
+  ],
+
+  transform: {
+    // '.+\\.(ts|tsx)$': 'ts-jest',
+    '.+\\.(ts|tsx)$': ['@swc/jest']
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+};
