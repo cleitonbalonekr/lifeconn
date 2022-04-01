6790a841b3346c39c63ab7a981b2f4d4682edc82
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-04-01 20:31:12 -0300

    feat(components): add ButtonLink component

---
+++ src/presentation/shared/components/form/link.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { useTailwind } from 'tailwind-rn';
+
+interface Props extends TouchableOpacityProps {
+  label: string;
+}
+
+const ButtonLink: React.FC<Props> = ({ label, ...rest }) => {
+  const tailwind = useTailwind();
+  return (
+    <TouchableOpacity style={tailwind('mt-1')} {...rest}>
+      <Text style={tailwind('text-blue-500')}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonLink;
