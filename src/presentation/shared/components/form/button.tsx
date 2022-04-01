bed6deb1bd751d7f050ed71794d34533d0c8a3e0
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-04-01 20:30:53 -0300

    feat(components): add button component

---
+++ src/presentation/shared/components/form/button.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View
+} from 'react-native';
+import { useTailwind } from 'tailwind-rn';
+
+type ButtonStatusType = 'success' | 'warning' | 'danger' | 'primary';
+
+interface Props extends TouchableOpacityProps {
+  label?: string;
+  type?: ButtonStatusType;
+}
+
+const Button: React.FC<Props> = ({
+  label,
+  children,
+  type = 'success',
+  ...rest
+}: Props) => {
+  const tailwind = useTailwind();
+
+  const getButtonColorByType = () => {
+    switch (type) {
+      case 'success':
+        return 'bg-emerald-600';
+      case 'warning':
+        return 'bg-yellow-600';
+      case 'danger':
+        return 'bg-red-600';
+      default:
+        return 'bg-blue-600';
+    }
+  };
+
+  return (
+    <TouchableOpacity
+      style={tailwind(
+        `flex flex-row p-3 items-center justify-center ${getButtonColorByType()} rounded`
+      )}
+      {...rest}
+    >
+      <View style={tailwind('pr-2')}>{children}</View>
+      {label && (
+        <Text style={tailwind('text-white font-semibold')}>{label}</Text>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
