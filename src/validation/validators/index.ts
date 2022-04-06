57931c3a8f5da0e74ae6853ff39536183e37c092
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-06 15:19:16 -0300

    feat(validation): add fullName validator

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,3 +1,4 @@
 export { default as email } from './email';
 export { default as password } from './password';
 export { default as phoneNumber } from './phoneNumber';
+export { default as fullName } from './fullName';
