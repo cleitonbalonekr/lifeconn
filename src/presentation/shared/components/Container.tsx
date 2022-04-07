77b9b743a1655e067c05934e648f7a04630fdf45
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-07 16:13:34 -0300

    style: improve Container styles

--- src/presentation/shared/components/Container.tsx
+++ src/presentation/shared/components/Container.tsx
@@ -12,7 +12,7 @@ const Container: React.FC<Props> = ({ children, scroll = false, ...rest }) => {
     <SafeAreaView style={tailwind('flex-1 bg-slate-100')}>
       <ScrollView
         style={tailwind('bg-slate-100')}
-        contentContainerStyle={tailwind('p-5  bg-slate-100')}
+        contentContainerStyle={tailwind('p-5 flex-grow  bg-slate-100')}
         {...rest}
       >
         {children}
