1b4e75cf58d336489d79982237afdd3cca877469
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-08 09:31:12 -0300

    styles(Contact): improve DetailsContact styles

--- src/presentation/modules/contacts/pages/DetailsContact.tsx
+++ src/presentation/modules/contacts/pages/DetailsContact.tsx
@@ -1,16 +1,23 @@
 import { Ionicons } from '@expo/vector-icons';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { useTailwind } from 'tailwind-rn';
 
 import Container from '@/presentation/shared/components/Container';
 import Button from '@/presentation/shared/components/form/button';
 import Input from '@/presentation/shared/components/form/input';
+import useInputState from '@/presentation/shared/hooks/useInputState';
 
 const DetailsContact: React.FC = () => {
   const tailwind = useTailwind();
-  const token =
-    '123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789';
+  const token = useInputState({
+    name: 'token'
+  });
+  useEffect(() => {
+    token.set(
+      '123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789'
+    );
+  }, []);
 
   return (
     <Container>
@@ -19,13 +26,13 @@ const DetailsContact: React.FC = () => {
           'flex flex-row border-b border-gray-300 items-center py-4 '
         )}
       >
-        <View style={tailwind('rounded-full bg-slate-300 p-2')}>
+        <View style={tailwind('rounded-full bg-slate-300 p-4')}>
           <Ionicons name="person-outline" size={20} />
         </View>
         <Text style={tailwind('text-lg text-center px-2')}>Fulano</Text>
       </View>
-      <Input placeholder="Token" label="Token" value={token} />
-      <View style={tailwind('flex-1 justify-center')}>
+      <View style={tailwind('mt-8 justify-center')}>
+        <Input placeholder="Token" label="Token" value={token.value} />
         <Button label="Remover" type="danger" onPress={() => {}}>
           <Ionicons
             name="trash-bin-outline"
