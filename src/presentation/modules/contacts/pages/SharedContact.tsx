739b65696cc12ffb2d4ee5967a25bfd08d163569
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-08 09:46:18 -0300

    feat(ShareContact): add share function

--- src/presentation/modules/contacts/pages/SharedContact.tsx
+++ src/presentation/modules/contacts/pages/SharedContact.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, Share } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import { useTailwind } from 'tailwind-rn';
 
 import Container from '@/presentation/shared/components/Container';
 import Input from '@/presentation/shared/components/form/input';
+import ButtonLink from '@/presentation/shared/components/form/link';
+
+const QR_CODE_SIZE = Dimensions.get('window').height * 0.5;
 
 const SharedContact: React.FC = () => {
   const tailwind = useTailwind();
   const token =
     '123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789123456789';
+  async function handleShare() {
+    await Share.share({
+      message: token
+    });
+  }
 
   return (
     <Container>
-      <Text style={tailwind('text-lg text-center')}>
-        Escanei o QR code abaixo
-      </Text>
-      <View style={tailwind('flex-1 justify-center items-center')}>
-        <QRCode value={token} size={250} />
+      <View style={tailwind('flex-1  justify-around')}>
+        <View style={tailwind('justify-center items-center')}>
+          <Text style={tailwind('text-lg text-center mb-4')}>
+            Escanei o QR code abaixo
+          </Text>
+          <QRCode value={token} size={QR_CODE_SIZE} />
+        </View>
+        <View style={tailwind(' justify-center items-center')}>
+          <Input placeholder="Token" label="Meu token" value={token} />
+          <ButtonLink
+            label="Compartilhar por outros meios"
+            onPress={handleShare}
+          />
+        </View>
       </View>
-      <Input placeholder="Token" label="Meu token" value={token} />
-      <Text style={tailwind('text-blue-500')}>
-        Compartilhar por outros meios
-      </Text>
     </Container>
   );
 };
