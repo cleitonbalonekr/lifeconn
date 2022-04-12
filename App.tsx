0c90a69869d6286506aba361f6ae4583335c31c2
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-04-11 21:04:14 -0300

    chore: configure firebase

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import Toast from 'react-native-toast-message';
 import { TailwindProvider } from 'tailwind-rn';
+import '@/configs/firebase';
 
 import Routes from '@/main/routes/index';
 import { AuthProvider } from '@/presentation/shared/context/auth';
---
+++ src/configs/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+initializeApp({
+  apiKey: 'AIzaSyChjMdA7xLTWb3CeUIbk_1dXNIDyE28C7M',
+  authDomain: 'lifeconn-4d4ff.firebaseapp.com',
+  projectId: 'lifeconn-4d4ff',
+  storageBucket: 'lifeconn-4d4ff.appspot.com',
+  messagingSenderId: '668458212425',
+  appId: '1:668458212425:android:68365b9c41363ff35c3398'
+});
