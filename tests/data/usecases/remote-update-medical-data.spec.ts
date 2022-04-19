7cf25648cb2fb8b9b32411cd7eb15198f8988e18
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-19 14:56:48 -0300

    refact(remote-delete-medical-data): rename sut

--- tests/data/usecases/remote-update-medical-data.spec.ts
+++ tests/data/usecases/remote-update-medical-data.spec.ts
@@ -10,33 +10,34 @@ import {
 const makeSut = () => {
   const updateUserMedicalDataRepository =
     new UpdateUserMedicalDataRepositorySpy();
-  const remoteAddMedicalData = new RemoteUpdateMedicalData(
+  const remoteUpdateMedicalData = new RemoteUpdateMedicalData(
     updateUserMedicalDataRepository
   );
   return {
-    remoteAddMedicalData,
+    remoteUpdateMedicalData,
     updateUserMedicalDataRepository
   };
 };
 
 describe('RemoteUpdateMedicalData', () => {
   it('it Should throw MedicalDataNotFound when does not found user to update medicalData', async () => {
-    const { updateUserMedicalDataRepository, remoteAddMedicalData } = makeSut();
+    const { updateUserMedicalDataRepository, remoteUpdateMedicalData } =
+      makeSut();
     updateUserMedicalDataRepository.response = null;
     const medicalData = {
       ...makeMedicalData(),
       id: fakeId
     };
-    const promise = remoteAddMedicalData.update(medicalData, fakeId);
+    const promise = remoteUpdateMedicalData.update(medicalData, fakeId);
     await expect(promise).rejects.toThrow(new MedicalDataNotFound());
   });
   it('it return AuthUser when update medical data with success', async () => {
-    const { remoteAddMedicalData } = makeSut();
+    const { remoteUpdateMedicalData } = makeSut();
     const medicalData = {
       ...makeMedicalData(),
       id: fakeId
     };
-    const response = await remoteAddMedicalData.update(medicalData, fakeId);
+    const response = await remoteUpdateMedicalData.update(medicalData, fakeId);
     expect(response).toHaveProperty('id');
     expect(response).toHaveProperty('medicalData');
   });
