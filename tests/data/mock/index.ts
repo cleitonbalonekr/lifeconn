6dfcd1d07bb45188612f8740db533a2a11a05d45
cleitonbalonekr <cleitonbaloneker@gmail.com>
2022-04-22 11:01:00 -0300

    feat(Http): add http client

--- tests/data/mock/index.ts
+++ tests/data/mock/index.ts
@@ -1,1 +1,4 @@
 export * from './account-mock';
+export * from './auth-mock';
+export * from './user-mock';
+export * from './mock-http';
---
+++ tests/data/mock/mock-http.ts
@@ -0,0 +1,38 @@
+import faker from '@faker-js/faker';
+
+import {
+  HttpRequest,
+  HttpResponse,
+  HttpStatusCode,
+  HttpClient
+} from '@/data/protocols/http';
+
+const object = { keyA: 'valueA', keyB: 42 };
+export const mockHttpRequest = (): HttpRequest => ({
+  url: faker.internet.url(),
+  method: faker.random.arrayElement(['get', 'post', 'put', 'delete']),
+  body: faker.random.objectElement(object),
+  headers: faker.random.objectElement(object)
+});
+
+export class HttpClientSpy<R = any> implements HttpClient<R> {
+  url?: string;
+
+  method?: string;
+
+  body?: any;
+
+  headers?: any;
+
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  };
+
+  async request(data: HttpRequest): Promise<HttpResponse<R>> {
+    this.url = data.url;
+    this.method = data.method;
+    this.body = data.body;
+    this.headers = data.headers;
+    return this.response;
+  }
+}
