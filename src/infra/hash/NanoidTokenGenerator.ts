18bc4b112a2d2e7862a3d5ca7d4e83e9d8d241e4
Cleiton Baloneker <cleitonbaloneker@gmail.com>
2022-04-28 10:39:56 -0300

    update(NanoidTokenGenerator): ensure that token have 6 caracteres

--- src/infra/hash/NanoidTokenGenerator.ts
+++ src/infra/hash/NanoidTokenGenerator.ts
@@ -4,10 +4,14 @@ import { TokenGenerator } from '@/data/protocols/hash/TokenGenerator';
 
 export class NanoidTokenGenerator implements TokenGenerator {
   generate(): string {
-    const token = nanoid(6)
-      .replaceAll('-', '')
-      .replaceAll('_', '')
-      .toLocaleLowerCase();
+    let token = '';
+
+    while (token.length < 6) {
+      token = nanoid(6)
+        .replaceAll('-', '')
+        .replaceAll('_', '')
+        .toLocaleLowerCase();
+    }
     return token;
   }
 }
